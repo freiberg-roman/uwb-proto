8f3290c624449da8f04d130aeb85e0f8c5918039
freiberg-roman <roman.sascha@googmail.com>
2021-06-25 14:07:03 +0200

    rec array

--- uwb/examples/main.py
+++ uwb/examples/main.py
@@ -2,6 +2,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from uwb.generator import BlobGenerator
+from uwb.map import NoiseMapGM
 
 
 @hydra.main(config_path="conf", config_name="main")
@@ -10,16 +11,15 @@ def run(cfg: DictConfig):
 
     # generate multimodal data for a simple grid
     gen = BlobGenerator(
-        [2, 2],
+        [2, 2, 3],
         cfg.generator.step_size,
         cfg.generator.measurements_per_location,
         (cfg.generator.modal_range[0], cfg.generator.modal_range[1]),
     )
-    data = gen.gen()
+    NoiseMapGM(gen)
 
     for d in gen:
         print(d[0].shape)
-    print(data.shape)
 
 
 if __name__ == "__main__":
--- uwb/map/noise_map_gm.py
+++ uwb/map/noise_map_gm.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from uwb.generator import BaseGenerator
 from uwb.map import NoiseMap
 
@@ -6,8 +8,26 @@ class NoiseMapGM(NoiseMap):
     """Provides estimates for each position with Gaussian Mixtures via DBSCAN preprocessing."""
 
     def __init__(self, measurement_generator: BaseGenerator):
+        """Pre-allocates data structures for parameter estimations.
+
+        This module is just a prove of concept and has to be rewritten if the task requires speed.
+        The simpler TODO ref class noise map normal should be used for now.
+        """
+        self.measurement_generator = measurement_generator
+
+        def _rec_array(shapes):
+            if not shapes:
+                return []
+            else:
+                first = shapes.pop(0)
+                return [_rec_array(deepcopy(shapes)) for _ in range(first)]
+
+        self.params = _rec_array(list(measurement_generator.shape))
+        print(self.params)
+
+    def gen(self):
         """Calculates estimates.
 
         If DBSCAN finds a cluster with less than three samples, they will be thrown away.
         """
-        self.measurement_generator = measurement_generator
+        pass
